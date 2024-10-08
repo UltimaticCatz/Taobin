24850bd5435fd5cc1ce90f30a21e19fcfbdb2ed0
ppparisorn <148557409+ppparisorn@users.noreply.github.com>
2024-10-08 18:08:14 +0700

    add database

--- database/mysql_database.sql
+++ database/mysql_database.sql
@@ -11,8 +11,13 @@ CREATE TABLE IF NOT EXISTS beverage (
 -- Insert data into the beverage table
 INSERT INTO beverage (drink_name, price, type_drink) 
 VALUES 
-    ('banana frappe', 30.00, 'frappe'), 
-    ('oreo frappe', 40.00, 'frappe'), 
-    ('hot cocoa', 30.00, 'hot'), 
-    ('cold cocoa', 35.00, 'cold'), 
-    ('cocoa frappe', 40.00, 'frappe');
+    ('Hot Jasmine Tea', 25.00, 'Hot'), 
+    ('Cold Jasmine Tea', 30.00, 'Cold'), 
+    ('Hot Jasmine Milk Tea', 30.00, 'Hot'), 
+    ('Cold Jasmine Milk Tea', 35.00, 'Cold'), 
+    ('Jasmine Milk Tea Frappe', 30.00, 'frappe'), 
+    ('Banana Frappe', 30.00, 'frappe'), 
+    ('Oreo Frappe', 40.00, 'frappe'), 
+    ('Hot Cocoa', 30.00, 'hot'), 
+    ('Cold Cocoa', 35.00, 'cold'), 
+    ('Cocoa Frappe', 40.00, 'frappe');
