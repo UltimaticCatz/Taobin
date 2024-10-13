360d5e9b0c3f313ef5f533b098172df90aba00fa
UltimaticCatz <onglee2004@hotmail.com>
2024-10-13 17:45:14 +0700

    updated drinks route (mostly finished). changed port of database to 3307)

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 
 //routes
-var usersRouter = require('./routes/users');
-var drinksRouter = require('/routes/drinks')
+var drinksRouter = require('/routes/drinks');
 
 //express app
 var app =  express();
@@ -63,6 +64,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //some express stuff
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(cors()) //use cors
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -74,10 +77,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html')); // Serve index.html
 });
 
-app.use('/users', usersRouter);
+//main routers calls
 app.use('/drinks', drinksRouter);
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/drinks.js
+++ backend/routes/drinks.js
@@ -1,26 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', (req, res, next) => {
-  const query = 'SELECT * from drinks';
-
+  const query = 'SELECT * from beverage';
+  console.log('querying from database with this query: ', query);
   req.dbConnection.query(query, (err, results) => {
     if (err) {
-      console.error("error querying from database: ", err.stack);
-      res.status(500).send("Error querying the database");
-      return;
+        console.error("error querying from database: ", err.stack);
+        return res.status(500).send("Error querying from database");
     }
-    res.json(results);  
-  });
+    res.status(201).json(results);  
+});
 });
 
 
 //havent test this yet
 router.post('/registerDrink', (req, res) => {
-    const {drink_name, cost} = req.body;
+    const {drink_name, cost, type_drink, image} = req.body;
     
-    const query = `INSERT into drinks (drink_name, cost) values (${drink_name}, ${cost})`;
+    const query = `INSERT into beverage (drink_name, price , type_drink, image) values (${drink_name}, ${cost}, ${type_drink}, ${image})`;
 
     req.dbConnection.query(query, (err, results) => {
         if (err) {
