94699649b58e6d77e3f5421d176b0a8039eaef21
Shazlow <boss2457bc@gmail.com>
2024-10-17 15:06:35 +0700

    Hero text change

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className='flex justify-center'>
         <div className='font-mono bg-orange-300 text-center text-8xl py-6 px-96 mt-10 mb-16 rounded-full'>
-            Taobin Sim
+            Taobin Simulator
         </div> 
     </div>
   )
